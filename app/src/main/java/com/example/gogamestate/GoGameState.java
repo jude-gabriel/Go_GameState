ebfee38f7660280b1a8eea837151d5d9aba22a12
jude-gabriel <judepgabriel@gmail.com>
2021-09-27 12:13:56 -0700

    Added private variables to stone class and moved enums

--- app/src/main/java/com/example/gogamestate/GoGameState.java
+++ app/src/main/java/com/example/gogamestate/GoGameState.java
@@ -2,11 +2,6 @@ package com.example.gogamestate;
 
 public class GoGameState {
 
-    public enum Color{
-        BLACK,
-        WHITE,
-        NONE
-    };
 
     //When true it is Player 1's turn
     //When false it is Player 2's turn
