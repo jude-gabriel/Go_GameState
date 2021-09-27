c4bac77742119811b60e956f6463b0aac90ef3bd
jude-gabriel <judepgabriel@gmail.com>
2021-09-27 12:02:50 -0700

    Added an int array for mapping the board and user click locations

--- app/src/main/java/com/example/gogamestate/GoGameState.java
+++ app/src/main/java/com/example/gogamestate/GoGameState.java
@@ -3,13 +3,20 @@ package com.example.gogamestate;
 public class GoGameState {
 
     public enum Color{
-        RED,
-        GREEN,
+        WHITE,
+        BLACK,
         NONE
     };
 
     //When true it is Player 1's turn
     //When false it is Player 2's turn
     private boolean userTurn;
+
+    //int array for locations on the board
+    private int[][] gameBoard;
+
+    //Float values for the users click locations
+    private float userXClick;
+    private float userYClick;
 
 }
