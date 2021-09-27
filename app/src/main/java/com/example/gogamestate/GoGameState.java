28972f31ef7bf03ce1958195b0c16472239954ad
jude-gabriel <judepgabriel@gmail.com>
2021-09-27 11:56:37 -0700

    Added Color enums and a boolean for the users turn.

---
+++ app/src/main/java/com/example/gogamestate/GoGameState.java
@@ -0,0 +1,15 @@
+package com.example.gogamestate;
+
+public class GoGameState {
+
+    public enum Color{
+        RED,
+        GREEN,
+        NONE
+    };
+
+    //When true it is Player 1's turn
+    //When false it is Player 2's turn
+    private boolean userTurn;
+
+}
