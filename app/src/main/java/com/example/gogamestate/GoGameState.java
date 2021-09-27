f2d97051134008b9c72b88415c87dcec76cf6b2c
jude-gabriel <judepgabriel@gmail.com>
2021-09-27 12:12:42 -0700

    Added private variables to stone class and moved enums

--- app/src/main/java/com/example/gogamestate/GoGameState.java
+++ app/src/main/java/com/example/gogamestate/GoGameState.java
@@ -2,18 +2,14 @@ package com.example.gogamestate;
 
 public class GoGameState {
 
-    public enum Color{
-        WHITE,
-        BLACK,
-        NONE
-    };
+
 
     //When true it is Player 1's turn
     //When false it is Player 2's turn
     private boolean userTurn;
 
-    //int array for locations on the board
-    private int[][] gameBoard;
+    //Stones array for locations on the board
+    private Stones[][] gameBoard;
 
     //Float values for the users click locations
     private float userXClick;
--- app/src/main/java/com/example/gogamestate/Stones.java
+++ app/src/main/java/com/example/gogamestate/Stones.java
@@ -2,5 +2,18 @@ package com.example.gogamestate;
 
 public class Stones {
 
+    public enum Color{
+        WHITE,
+        BLACK,
+        NONE
+    };
+
+    //Create private variables for the x and y locations of the of the stones
+    private float xLocation;
+    private float yLocation;
+
+    //Create a private int for the radius of stones
+    private int radius;
+
 
 }
