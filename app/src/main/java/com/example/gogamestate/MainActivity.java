7538791a89eb91c2953c6327741fdf92c16cf933
andersmi24 <andersmi24@up.edu>
2021-09-29 21:47:37 -0700

    Added runTest button

--- app/src/main/java/com/example/gogamestate/MainActivity.java
+++ app/src/main/java/com/example/gogamestate/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.gogamestate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainActivity main = new MainActivity();
+
+        GoGameState gameState = new GoGameState();
+
+        Button runTest = (Button) findViewById(R.id.runTest);
+        runTest.setOnClickListener(main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
