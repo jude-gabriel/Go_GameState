c307871b4daa89d0da22eaf32fd1cc3d2fd6f16b
BH <74618590+harringt23@users.noreply.github.com>
2021-10-05 11:55:33 -0600

    changed testing to seekbar

--- app/src/main/java/com/example/gogamestate/MainActivity.java
+++ app/src/main/java/com/example/gogamestate/MainActivity.java
@@ -6,10 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 /* ERRORS
- * TODO: issue with scoring and resting the score
  */
 public class MainActivity extends AppCompatActivity{
 
@@ -36,6 +36,64 @@ public class MainActivity extends AppCompatActivity{
         // initialize objects for the buttons to test
         Button runTest = findViewById(R.id.runTest);
         Button forfeit = findViewById(R.id.forfeitTest);
+        SeekBar tests = findViewById(R.id.testBar);
+
+        // override the seekbar to perform each test
+        // @author Brynn Harrington
+        tests.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int prog = seekBar.getProgress();
+                switch (prog) {
+                    // Capture Test
+                    // @author Jude Gabriel
+                    case 0:
+                        if (gameState != null) {
+                            //Initialize the empty board
+                            if (counter == 0) {
+                                theText.setText(gameState.toString());
+                            }
+
+                            //Prepopulate with one stone missing from capture
+                            if (counter == 1) {
+                                gameState.testCaptures();
+                                theText.setText(gameState.toString());
+                            }
+
+                            if (counter == 2) {
+                                boolean move = gameState.playerMove(600, 750);
+                                theText.setText(gameState.toString());
+                            }
+                            if (counter == 3) {
+                                boolean move = gameState.playerMove(600, 400);
+                                theText.setText(gameState.toString());
+                            }
+                            if (counter == 4) {
+                                boolean move = gameState.playerMove(950, 400);
+                                theText.setText(gameState.toString());
+                            }
+                            counter++;
+                        }
+                        break;
+                    // Forfeit Test
+                    case 1:
+                        //Call the forfeit test function
+                        gameState.testForfeit();
+                        theText.setText(gameState.toString());
+                        break;
+                    case 2:
+                        break;
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
 
         // override the run test
         // @author Jude Gabriel
